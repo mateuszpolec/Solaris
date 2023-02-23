ade9a8aabd0b0306676a7cef9589af721a1d400d
Mateusz <316072@uwr.edu.pl>
2023-02-23 18:45:39 +0100

    Init project

---
+++ Source/Solaris/Solaris.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Solaris : ModuleRules
+{
+	public Solaris(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
